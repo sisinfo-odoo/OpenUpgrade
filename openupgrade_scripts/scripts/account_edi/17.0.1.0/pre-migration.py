53a4bd666fc987e9519ad8b759417e3068667c8f
Alexis <acp@microcom.ca>
2024-10-16 15:50:06 -0400

    [17.0][OU-ADD] account_edi: Migration to 17.0

--- openupgrade_scripts/scripts/account_edi/17.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/account_edi/17.0.1.0/pre-migration.py
@@ -6,11 +6,15 @@ from openupgradelib import openupgrade
 
 def _remove_table_constraints(env):
     openupgrade.delete_sql_constraint_safely(
-        env, "account_edi", "account_edi_document",
+        env,
+        "account_edi",
+        "account_edi_document",
         "account_edi_document_unique_edi_document_by_move_by_format"
     )
     openupgrade.delete_sql_constraint_safely(
-        env, "account_edi", "account_edi_format",
+        env,
+        "account_edi",
+        "account_edi_format",
         "account_edi_format_unique_code"
     )
 
