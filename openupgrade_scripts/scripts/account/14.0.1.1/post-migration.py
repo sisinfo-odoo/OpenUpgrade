12621ef2973f31f25dadd5b27383d85ef4979333
Miguel Gómez León (SISINFO) <mgomezleon@sisinfo.com.uy>
2025-02-06 10:31:44 -0300

    [ADD][-][-] COMENTANDO DEFINITIVAMENTE 'def post_statements' por ser invocado en 14.0 scripts/account/14.0.1.1/post-migration.py

--- openupgrade_scripts/scripts/account/14.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/account/14.0.1.1/post-migration.py
@@ -316,26 +316,27 @@ def create_account_tax_report_lines(env):
 def post_statements(env):
     """These 2 inconsistent use cases require to put the statement as posted."""
     # Confirmed statements that have unreconciled items
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE account_bank_statement bst
-        SET state = 'posted'
-        FROM account_bank_statement_line bstl
-        WHERE bst.state = 'confirm' AND bstl.statement_id = bst.id
-            AND bstl.is_reconciled IS DISTINCT FROM TRUE
-        """,
-    )
-    # New statements with reconciled items
-    openupgrade.logged_query(
-        env.cr,
-        """SELECT bst.id FROM account_bank_statement bst
-        JOIN account_bank_statement_line bstl ON bstl.statement_id = bst.id
-        WHERE bst.state = 'open' AND bstl.is_reconciled IS TRUE
-        """,
-    )
-    stmt_ids = list({x[0] for x in env.cr.fetchall()})
-    env["account.bank.statement"].browse(stmt_ids).button_post()
+    pass
+    # openupgrade.logged_query(
+    #     env.cr,
+    #     """
+    #     UPDATE account_bank_statement bst
+    #     SET state = 'posted'
+    #     FROM account_bank_statement_line bstl
+    #     WHERE bst.state = 'confirm' AND bstl.statement_id = bst.id
+    #         AND bstl.is_reconciled IS DISTINCT FROM TRUE
+    #     """,
+    # )
+    # # New statements with reconciled items
+    # openupgrade.logged_query(
+    #     env.cr,
+    #     """SELECT bst.id FROM account_bank_statement bst
+    #     JOIN account_bank_statement_line bstl ON bstl.statement_id = bst.id
+    #     WHERE bst.state = 'open' AND bstl.is_reconciled IS TRUE
+    #     """,
+    # )
+    # stmt_ids = list({x[0] for x in env.cr.fetchall()})
+    # env["account.bank.statement"].browse(stmt_ids).button_post()
 
 
 def pass_bank_statement_line_note_to_journal_entry_narration(env):
