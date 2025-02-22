621cb41282fd3ab86410b0c4794e3bcaad0025c0
Miguel Gómez León (SISINFO) <mgomezleon@sisinfo.com.uy>
2025-02-22 14:39:21 -0300

    [ADD][-][-] Renombrando "hr_contract_history" a "hr_contract_history_table" evitando conflictos

--- openupgrade_scripts/scripts/hr_contract/15.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/hr_contract/15.0.1.0/pre-migration.py
@@ -6,7 +6,18 @@ _columns_copy = {
     ],
 }
 
+_model_renames = [
+    ("hr.contract.history", "hr.contract.history_table"),
+]
+
+_table_renamed = [
+    ("hr_contract_history", "hr_contract_history_table"),
+]
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.copy_columns(env.cr, _columns_copy)
+
+    # Se hace esto porque en los custom de BPS ya existe este modelo, entonces se pasa para otro y asi evitar conflicto con Odoo
+    openupgrade.rename_models(env.cr, _model_renames)
+    openupgrade.rename_tables(env.cr, _table_renamed)
